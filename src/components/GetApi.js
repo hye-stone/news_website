03d0b38cf74d79ed4393cd6eb00daa2513e57037
Kang Hye-Seok <93122344+hye-stone@users.noreply.github.com>
2022-06-29 02:01:59 +0900

    Update GetApi.js

--- src/components/GetApi.js
+++ src/components/GetApi.js
@@ -37,7 +37,7 @@ const GetAPI = (): ReactElement => {
 
     useEffect(() => {
         if (dataTechnology.length === 0) {
-            // console.log(dataTechnology);
+            // console.log(dataTechnology); 
             fetchData1();
         }
     }, [fetchData1, dataTechnology]);
